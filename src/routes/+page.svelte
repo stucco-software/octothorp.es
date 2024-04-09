2e0ba817f34570e6624a2539a217605f78c8aadc
nikolaswise <nikolaswise@gmail.com>
2024-04-09 09:49:09 +0200

    hopepage and terms

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,55 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Octothorpes</h1>
+
+<p>Hashtags for Websites.</p>
+
+<h2>How It Works</h2>
+
+<ol>
+  <li>
+    <a href="/register">Register your domain</a> with the Octorthorpes community.
+  </li>
+  <li>
+    Add the `TXT` record to your DNS to verify that it's _your_ domain.
+  </li>
+  <li>
+    <a href="/domains">Check back</a> to see when your domain is approved.
+  </li>
+  <li>
+    Add a link on your website to an <a href="/#octothorpe">#octothorpe</a>.
+  </li>
+  <li>
+    Follow that link to register it.
+  </li>
+</ol>
+
+
+<h2>Terms</h2>
+
+<p>
+  Octothorpes is a community-run project with explicit community-run ideals. This project is anti-racist, feminist, and anti-fascist. Hateful, racsist, and inflammatory content and octothorpes are disallowed. Our team will review octothoropes and domains and reserve the right to make the final determination on if the content is against our terms.
+</p>
+
+<ul>
+  <li>
+    <b>
+      We will ban any domains with contnet that violates this policy.
+    </b>
+  </li>
+  <li>
+    <b>
+      We will ban any octothorpes that violate this policy.
+    </b>
+  </li>
+  <li>
+    <b>
+      We will ban any domain that originates an octothorpe that violate this policy.
+    </b>
+  </li>
+  <li>
+    <b>We will automatically ban any domain that reuses a previously banend octorhorpe.</b>
+  </li>
+</ul>
+
+<p>
+  This is a space for our community, and you're invited to be a part of it. We hold all our communuty members to the same standard.
+</p>
