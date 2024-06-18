4ea7abf8564960567750ee749897dc87a9da0769
nikolaswise <nikolaswise@gmail.com>
2024-06-18 10:59:17 +0200

    thorpe page

---
+++ src/routes/~/+page.server.js
@@ -0,0 +1,22 @@
+import { queryBoolean, queryArray, insert } from '$lib/sparql.js'
+import { instance } from '$env/static/private'
+
+export async function load(req) {
+  // get all the relevant thorpes
+  let thorpes = []
+  try {
+    const sr = await queryArray(`
+      SELECT DISTINCT ?t {
+        ?t rdf:type <octo:Term> .
+      }
+    `)
+    thorpes = new Set(sr.results.bindings
+      .map(b => b.t.value)
+      .filter(t => t.includes(instance)))
+  } catch (e) {
+    console.log(e)
+  }
+  return {
+    thorpes
+  }
+}
---
+++ src/routes/~/+page.svelte
@@ -0,0 +1,38 @@
+<script type="text/javascript">
+  export let data
+  let thorpes = [...data.thorpes]
+  let value
+  $: {
+    thorpes = value
+      ? [...data.thorpes].filter(n => n.includes(value))
+      : [...data.thorpes]
+  }
+</script>
+
+<form>
+  <label
+    for="value">
+    Filter
+  </label>
+  <input
+    placeholder="#…"
+    type="text"
+    bind:value={value}
+    name="filter">
+</form>
+
+{#each thorpes as thorpe}
+  <a class="thorpe" href="{thorpe}">#{thorpe.split('/~/')[1]}</a>
+{/each}
+
+<style type="text/css">
+  form {
+    margin-block-end: var(--baseline);
+  }
+  input {
+    width: 100%;
+  }
+  a {
+    margin-inline-end: 1ch;
+  }
+</style>
