78f3739fe7e8c2be5ff26bbb19940d60cf537d82
nikolaswise <nikolaswise@gmail.com>
2024-08-17 21:15:19 -0700

    unfuck the flyer

--- src/routes/flyer/+page.svelte
+++ src/routes/flyer/+page.svelte
@@ -1,34 +1,46 @@
 
-<div class="container" style="width: 100%; background: url(/code-bg.png) left no-repeat; background-size: contain;">
-<p>Thank you for directing your browser to the kinda-secret launch of <a href="https://octothorp.es/">Octothorpes</a>.</p>
+<div class="container">
+  <img src="/code-bg.png">
 
-<p>Octothorpes are hashtags and backlinks you can use on regular websites. Independent sites linked by a ring can see each other's hastags and links, and we've opened up the first ring just for XOXO.</p>
+  <p>Thank you for directing your browser to the kinda-secret launch of <a href="https://octothorp.es/">Octothorpes</a>.</p>
 
-<p>If you didn't get the spiel about what they are and how to use them, the <a href="https://demo.ideastore.dev/">demo site</a> is a good introduction. You can also check out the octothorpe for <a href="https://octothorp.es/~/demo">demo</a>.</p>
+  <p>Octothorpes are hashtags and backlinks you can use on regular websites. Independent sites linked by a ring can see each other's hashtags and links, and we've opened up the first ring just for XOXO.</p>
 
-<h3>What did I win?</h3>
+  <p>If you didn't get the spiel about what they are and how to use them, the <a href="https://demo.ideastore.dev/">demo site</a> is a good introduction. You can also check out the octothorpe for <a href="https://octothorp.es/~/demo">demo</a>.</p>
 
-<p>Nothing! Ok, well, something. If you look under your chair, you'll find early access to the project. <strong>Just for the weekend of XOXO, any site can post to the Octothorpe network.</strong> (please be nice)</p>
+  <h3>What did I win?</h3>
 
-<h3>Join the world's slowest social network</h3>
+  <p>Nothing! Ok, well, something. If you look under your chair, you'll find early access to the project. <strong>Just for the weekend of XOXO, any site can post to the Octothorpe network.</strong> (please be nice)</p>
 
-<p><strong>If you have a website you control:</strong> You can start using Octothorpes by adding a small bit of code to the head of the page. Check out the Quickstart Guide, which includes a Wordpress plugin and non-javascript options. If you want to get more technical, we've got some docs on the main site.</p>
+  <h3>Join the world's slowest social network</h3>
 
-<p>Honestly though, just try to find Nim or Nik and they'll set you up.</p>
+  <p><strong>If you have a website you control:</strong> You can start using Octothorpes by adding a small bit of code to the head of the page. Check out the Quickstart Guide, which includes a Wordpress plugin and non-javascript options. If you want to get more technical, we've got some docs on the main site.</p>
 
-<p><strong>If you don't:</strong> Glitch.com can fix that! If you have a Glitch account, you can <a href="http://octothorpenty.glitch.me/">remix our starter site</a>. It's all set up to micro- and macro-blog using Octothorpes.</p>
+  <p>Honestly though, just try to find Nim or Nik and they'll set you up.</p>
 
-<h3>Why Tweet or Toot When you can Oof!</h3>
+  <p><strong>If you don't:</strong> Glitch.com can fix that! If you have a Glitch account, you can <a href="http://octothorpenty.glitch.me/">remix our starter site</a>. It's all set up to micro- and macro-blog using Octothorpes.</p>
 
-<p>Octothorpes doesn't store any data, but just pointing at a web site can get you a microblog's worth of content. So we built a <a href="http://octothorpenty.glitch.me/oofs">feed</a> for  <octo-thorpe>xoxo24</octo-thorpe></p>
+  <h3>Why Tweet or Toot When you can Oof!</h3>
 
-<p>Oof is yet another <a href="https://demo.ideastore.dev/synonyms">synonym</a> for "hashtag" so, you know, oof it up.</p>
+  <p>Octothorpes doesn't store any data, but just pointing at a web site can get you a microblog's worth of content. So we built a <a href="http://octothorpenty.glitch.me/oofs">feed</a> for  <octo-thorpe>xoxo24</octo-thorpe></p>
 
-<h2>Stay in Touch</h2>
+  <p>Oof is yet another <a href="https://demo.ideastore.dev/synonyms">synonym</a> for "hashtag" so, you know, oof it up.</p>
 
-<p>If you want to stay involved after XOXO, please fill out this [form] and we'll keep you up to date.</p>
+  <h2>Stay in Touch</h2>
 
-<p>You can find us on the XOXO slack or Mastodon but also if you make inventive Octothorpes, we <em>will</em> witness you.</p>
+  <p>If you want to stay involved after XOXO, please fill out this [form] and we'll keep you up to date.</p>
 
-
+  <p>You can find us on the XOXO slack or Mastodon but also if you make inventive Octothorpes, we <em>will</em> witness you.</p>
 </div>
+
+<style type="text/css">
+  img {
+    float: left;
+    width: 50%;
+    margin-right: -20%;
+  }
+  .container {
+    width: 100%;
+    background: white;
+  }
+</style>
