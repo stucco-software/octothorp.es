19c7f31908cb47d12970f0bd0b11d4191495a602
nikolaswise <nikolaswise@gmail.com>
2024-08-19 07:27:14 -0700

    flyer

--- src/routes/flyer/+page.svelte
+++ src/routes/flyer/+page.svelte
@@ -1,9 +1,11 @@
 <div class="container">
-  <img src="/code-bg.png">
+  <h1>
+    #xoxo2024
+  </h1>
 
   <p>Thank you for directing your browser to the kinda-secret launch of <a href="https://octothorp.es/">Octothorpes</a>.</p>
 
-  <p>Octothorpes are hashtags and backlinks you can use on regular websites. Independent sites linked by a ring can see each other's hashtags and links, and we've opened up the first ring just for XOXO.</p>
+  <p><strong>Octothorpes are hashtags and backlinks you can use on regular websites.</strong> Independent sites linked by a ring can see each other's hashtags and links, and we've opened up the first ring just for XOXO.</p>
 
   <p>If you didn't get the spiel about what they are and how to use them, the <a href="https://demo.ideastore.dev/">demo site</a> is a good introduction. You can also check out the octothorpe for <a href="https://octothorp.es/~/demo">demo</a>.</p>
 
@@ -13,9 +15,10 @@
 
   <h3>Join the world's slowest social network</h3>
 
-  <p><strong>If you have a website you control:</strong> You can start using Octothorpes by adding a small bit of code to the head of the page. Check out the Quickstart Guide, which includes a Wordpress plugin and non-javascript options. If you want to get more technical, we've got some docs on the main site.</p>
+  <p><strong>If you have a website you control:</strong> You can start using Octothorpes by adding a small bit of code to the head of the page. Check out the <a href="https://demo.ideastore.dev/quickstart">Quickstart Guide</a> which includes a Wordpress plugin and non-javascript options. If you want to get more technical, we've got some docs on the main site.</p>
 
   <p>Honestly though, just try to find Nim or Nik and they'll set you up.</p>
+  <p>They're <strong>@nk</strong> or <strong>@nim</strong> on both the XOXO slack and <a href="https://xoxo.zone/">xoxo.zone</a>.</p>
 
   <p><strong>If you don't:</strong> Glitch.com can fix that! If you have a Glitch account, you can <a href="http://octothorpenty.glitch.me/">remix our starter site</a>. It's all set up to micro- and macro-blog using Octothorpes.</p>
 
@@ -27,19 +30,31 @@
 
   <h2>Stay in Touch</h2>
 
-  <p>If you want to stay involved after XOXO, please fill out this [form] and we'll keep you up to date.</p>
+  <p>If you want to stay involved after XOXO, please fill out this <a href="https://docs.google.com/forms/d/e/1FAIpQLSetdzsQtUofYIOdIE3LDSbcaGTERcFhayWS7CdE_PJfooVU5Q/viewform?usp=sf_link">form</a> and we'll keep you up to date.</p>
 
-  <p>You can find us on the XOXO slack or Mastodon but also if you make inventive Octothorpes, we <em>will</em> witness you.</p>
+  <p>You can always find us on the XOXO slack or <a href="https://xoxo.zone/">Mastodon</a> as <a href="https://xoxo.zone/@nk">@nk</a> or <a href="https://xoxo.zone/@nim">@nim</a> but also if you make inventive Octothorpes, we <em>will</em> witness you.</p>
 </div>
 
+
 <style type="text/css">
-  img {
-    float: left;
-    width: 50%;
-    margin-right: -20%;
-  }
   .container {
     width: 100%;
     background: white;
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    padding-inline: 2ch;
+    background: url(code-bg.png) no-repeat;
+    background-size: cover;
+  }
+  .container * {
+    background: white;
+    outline: 0.5ch solid white;
+  }
+  h1,
+  h2,
+  h3 {
+    margin-block-start: calc( 2 * var(--baseline) );
   }
 </style>
