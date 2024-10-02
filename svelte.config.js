3229663d881a182ab737d8f5bfa3044a72106557
nikolaswise <nikolaswise@gmail.com>
2024-10-01 20:08:13 -0700

    try thgis?

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,9 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-		csrf: false
+		csrf: {
+			checkOrigin: false
+		}
 	},
   extensions: [".svelte", ".md"],
   preprocess: [mdsvex(mdsvexConfig)]
