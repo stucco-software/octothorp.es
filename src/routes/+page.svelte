4fee4cd1a7a6e76342e80b6c2accfbb28236decf
Ním Wunnan <nim@wunnan.com>
2024-10-30 23:03:27 -0700

    card-ifys front page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,15 +12,20 @@
 
 
 
-<section class="dashboard dotgrid">
-  <p>
-    Welcome to the first Octothorpes ring. 
-  </p>
+<div class="dashboard dotgrid">
+<section>
+  <p>Welcome to the first Octothorpes ring.</p>
+  <hr />
+  <em>News from the Ring</em>
+    <a href="/~/weirdweboctober">Weird Web October has almost wrapped! Last day to post!</a>
 
-  <p>
-   This Octothorpes Ring is a network of <mark>{data.thorpes}</mark> <a href="/~">tags</a>, <mark>{data.links}</mark> <a href="/backlinks">backlinks</a> and <mark>{data.assertions}</mark> <a href="/bookmarks">bookmarks</a> across <mark>{data.domains}</mark> <a href="/domains">websites.</a>
-  </p>
 </section>
+<section>
+  <p>
+    This Octothorpes Ring is a network of <mark>{data.thorpes}</mark> <a href="/~">tags</a>, <mark>{data.links}</mark> <a href="/backlinks">backlinks</a> and <mark>{data.assertions}</mark> <a href="/bookmarks">bookmarks</a> across <mark>{data.domains}</mark> <a href="/domains">websites.</a>
+   </p> 
+</section>
+</div>
 
 <style type="text/css">
   h1 {
@@ -30,13 +35,24 @@
     display: grid;
     grid-template-columns: repeat(auto-fill, 24ch);
     gap: 4ch;
-    grid-auto-rows: minmax(0, auto);
+    grid-auto-rows: max-content
   }
 
-  .dashboard p {
+  .dashboard {
     /* border-top: 16px solid var(--bg-color); */
-    padding: 1ch;
     font-family: "DOS";
 
+  }
+  .dashboard section, .dashboard p span {
+  background-color: lightgoldenrodyellow;
+  outline: 1ch solid var(--bg-color);
+}
+  .dashboard section {
+    padding: 1ch 1ch 2ch 1ch;
+    border-radius: 2px;
+  }
+
+  hr {
+    border: 2px dashed var(--bg-color);
   }
 </style>
