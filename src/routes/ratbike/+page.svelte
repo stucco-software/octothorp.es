38382ef2898f3e9447b699fb8fe02c70800f737c
Ním Wunnan <nim@wunnan.com>
2024-10-31 00:04:55 -0700

    ratbike pr updates

--- src/routes/ratbike/+page.svelte
+++ src/routes/ratbike/+page.svelte
@@ -4,7 +4,7 @@
   <p>Rat bikes are motorcycles that have fallen apart over time and have been kept on the road and maintained for little or no cost by employing kludge fixes and improvised repairs, with little or no consideration given to appearance.</p>
 </blockquote>
 
-<p>Octothorpes development has been a little like building a car while we’re driving it, and so far we've strapped an engine to a couple wheels, which is more like a motorcycle. We want to test this in the real world as soon and as often as possible, so the level of polish we're aiming for in our first public trial is "Ratbike that doesn't break or hurt anyone."</p>
+<p>Octothorpes development has been a little like building a car while we're driving it, and so far we've strapped an engine to a couple wheels, which is more like a motorcycle. We want to test this in the real world as soon and as often as possible, so the level of polish we're aiming for in our first public trial is "Ratbike that doesn't break or hurt anyone."</p>
 
 <h2>Join us on this ride</h2>
 
@@ -12,11 +12,11 @@
 
 <h2>Tag freely, tag often</h2>
 
-<p>Octothorpes is a protocol, not a platform. So what goes on the network is up to you. We have a newsletter and an official hashtag for <a href="https://octothorp.es/~/octothorpe-news">#octothorpes-news</a> and some social media accounts where we’ll be sharing updates to the project as well as cool stuff we see and find on the network, but beyond that, what one might find clicking on any octothorpe will be up to those of you who choose to octothorpe your posts. If you want to organize any sort of hashtag-based project or campaign and want help in spreading the idea, get in touch. Otherwise… browse and discover!</p>
+<p>Octothorpes is a protocol, not a platform. So what goes on the network is up to you. We have a newsletter and an official hashtag for <a href="https://octothorp.es/~/octothorpe-news">#octothorpes-news</a> and some social media accounts where we'll be sharing updates to the project as well as cool stuff we see and find on the network, but beyond that, what one might find clicking on any octothorpe will be up to those of you who choose to octothorpe your posts. If you want to organize any sort of hashtag-based project or campaign and want help in spreading the idea, get in touch. Otherwise… browse and discover!</p>
 
 <h3>Then what?</h3>
 
-<p>After Ratbike ends, we’re going to take what we’ve learned and focus on the best way to get to a fully-public rollout that is both stable and sustainable. We’re committed to making sure the protocol remains free and open source. The main thing we want is for the protocol to thrive in the outside world.</p>
+<p>After Ratbike ends, we're going to take what we've learned and focus on the best way to get to a fully-public rollout that is both stable and sustainable. We're committed to making sure the protocol remains free and open source. The main thing we want is for the protocol to thrive in the outside world.</p>
 
 <p>We also want to be able to continue working on this, and are looking for sustainable, ethical ways to make that a job that could support the project, our families, and the broader community. What would that look like? We don't know yet, but probably a separate business that creates octothorpe-enabled services that are useful or convenient enough to people that they'd pay a little for them, like dashboards or hosted Rings. It <em>won't</em> look like a slow enshittification of core features as the paid services boundary creeps forward. How will we get there? Maybe a kickstarter or a partnership with an established project or company that aligns with our values, or maybe we just toil for another few years to reach that point. However it goes, we're going to stay transparent and communicative at every step of the journey.</p>
 
@@ -30,7 +30,7 @@
 <li>All data will go to a shared DB</li>
 <li>You can opt-in to having your data deleted at the end of the project</li>
 <li>At the end of Ratbike, or as soon as fully-independent Rings are supported, we will offer data export </li>
-<li>To be clear, <strong>no personal data will ever be collected</strong>. The DB stores Octothorpe Statements as RDF triples. Everything is just links. We don’t even record your IP address.</li>
+<li>To be clear, <strong>no personal data will ever be collected</strong>. The DB stores Octothorpe Statements as RDF triples. Everything is just links. We don't even record your IP address.</li>
 <li>Everything is public. Octothorpes link public web pages to other public web pages, and we have no encryption or privacy measures in place to hide the links. Don't put anything on the network that you don't want to be public.</li>
 <li>We don't have any promises on when we will support a certain feature or platform. But you <em>can</em> check out the <a href="https://github.com/orgs/stucco-software/projects/3/views/2">Roadmap</a> to see what we're working on, comment, and maybe even get involved.</li>
 <li>This is v 0.x of the protocol. It's possible that breaking changes will be made during Ratbike, but only if strictly necessary. We will have v 1 when Ratbike concludes.</li>
@@ -40,19 +40,19 @@
 
 <p>Q: How do I start using Octothorpes?</p>
 
-<p>A: First, request to register your url. Then, check out the quickstart guide. If you still have questions, hit us up on <a href="https://hachyderm.io/@octothorpes">Mastodon</a>,  <a href="https://bsky.app/profile/octothorpes.bsky.social">Bluesky</a>or <a href="mailto:info@octothorp.es">email</a>.</p>
+<p>A: First, request to register your url. Then, check out the <a href="https://demo.ideastore.dev/quickstart"></a>quickstart guide. If you still have questions, hit us up on <a href="https://hachyderm.io/@octothorpes">Mastodon</a>,  <a href="https://bsky.app/profile/octothorpes.bsky.social">Bluesky</a> or <a href="mailto:info@octothorp.es">email</a>.</p>
 
 <p>Q: Can I still register?</p>
 
-<p>A: <a href="https://octothorp.es/~/octothorpe-news">If this page</a> says you can, you can. If this says you can’t, we’re at capacity.</p>
+<p>A: <a href="https://octothorp.es/~/octothorpe-news">If this page</a> says you can, you can. If this says you can't, we're at capacity.</p>
 
 <p>Q: You mention Rings. Can I start my own Web Ring or convert my existing one?</p>
 
-<p>A: Not yet, but we’re working on it. See the <a href="https://github.com/orgs/stucco-software/projects/3/views/2?pane=issue&amp;itemId=84746160&amp;issue=stucco-software%7Coctothorp.es%7C60">Roadmap</a></p>
+<p>A: Not yet, but we're working on it. See the <a href="https://github.com/orgs/stucco-software/projects/3/views/2?pane=issue&amp;itemId=84746160&amp;issue=stucco-software%7Coctothorp.es%7C60">Roadmap</a></p>
 
-<p>Q: There’s another way I think you should do something. How should I tell you?</p>
+<p>Q: There's another way I think you should do something. How should I tell you?</p>
 
-<p>A: See the Roadmap. It’s full of github issues for things that we have and haven’t started working on yet.  </p>
+<p>A: See the Roadmap. It's full of github issues for things that we have and haven't started working on yet.  </p>
 
 <p>Q: Can I use Octothorpes without adding your code to my page?</p>
 
@@ -60,15 +60,15 @@
 
 <p>Q: I want to contribute to the project. How can I do that?</p>
 
-<p>A: Awesome! We don’t have a formal process yet, so just grab the repo and get in touch with Nim or Nik and we’ll get you going.</p>
+<p>A: Awesome! We don't have a formal process yet, so just grab the repo and get in touch with Nim or Nik and we'll get you going.</p>
 
 <p>Q: I think I found a bug. How can I report it?</p>
 
-<p>A: First, check to see if we know about it already at the <a href="https://github.com/orgs/stucco-software/projects/3/views/3">Bugs</a> list. If not, create a new issue and tag it “bug.”</p>
+<p>A: First, check to see if we know about it already at the <a href="https://github.com/orgs/stucco-software/projects/3/views/3">Bugs</a> list. If not, create a new issue and tag it <code>bug</code>.</p>
 
-<p>Q: How can I find out what’s going on with Octothorpes?</p>
+<p>Q: How can I find out what's going on with Octothorpes?</p>
 
-<p>A: Join the newsletter, follow #octothorpe-news, or #ratbike-news and find us on  <a href="https://hachyderm.io/@octothorpes">Mastodon</a> or  <a href="https://bsky.app/profile/octothorpes.bsky.social">Bluesky</a>.</p>
+<p>A: Join the <a href="https://octothorpe-news.beehiiv.com/subscribe" target="_blank">newsletter</a> follow #octothorpe-news, or #ratbike-news and find us on  <a href="https://hachyderm.io/@octothorpes">Mastodon</a> or  <a href="https://bsky.app/profile/octothorpes.bsky.social">Bluesky</a>.</p>
 
 <p>Q: Who are you guys?</p>
 
