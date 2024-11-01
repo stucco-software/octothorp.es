7bffda1444a43aa778c9706f090df2b20f455c87
Ním Wunnan <nim@wunnan.com>
2024-11-01 14:19:45 -0700

    adds testimonials

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script type="text/javascript">
   import TKCard from '$lib/components/TKCard.svelte'
   export let data
+  import { assets } from '$app/paths';
   let rss = `${data.instance}rss/`
 </script>
 
@@ -16,14 +17,30 @@
 <section>
   <p>Welcome to the first Octothorpes ring.</p>
   <p># # # # # # # # # # # </p>
-  <em>News from the Ring</em>
-    <a href="/~/weirdweboctober">Weird Web October has almost wrapped! Last day to post!</a>
-
+  <p>
+    This Ring is a network of <mark>{data.thorpes}</mark> <a href="/~">tags</a>, <mark>{data.links}</mark> <a href="/backlinks">backlinks</a> and <mark>{data.assertions}</mark> <a href="/bookmarks">bookmarks</a> across <mark>{data.domains}</mark> <a href="/domains">websites.</a>
+   </p> 
+  <p>   <a href="https://octothorp.es"><img src="{assets}/badge.png" alt="Badge for the Octothorpes main ring" /></a>
+  </p>
 </section>
 <section>
-  <p>
-    This Octothorpes Ring is a network of <mark>{data.thorpes}</mark> <a href="/~">tags</a>, <mark>{data.links}</mark> <a href="/backlinks">backlinks</a> and <mark>{data.assertions}</mark> <a href="/bookmarks">bookmarks</a> across <mark>{data.domains}</mark> <a href="/domains">websites.</a>
-   </p> 
+  <em>News from the Ring</em>
+  <p># # # # # # # # # # # </p>
+
+ <p>Registrations are open again for <a href="/ratbike">Project Ratbike</a></p>
+ <p># # # # # # # # # # # </p>
+   <a href="/~/weirdweboctober">Weird Web October has wrapped! Revel in the fun that was WWO!</a>
+
+</section>
+<section class="testimonial">
+  <p>The latest web nerd feature.</p>
+  <p class="attribution">-- Anil Dash</p>
+
+  <p>You've built something that has enriched a human life a little bit! That's rad as hell.</p>
+  <p class="attribution">-- <a href="https://mastodon.sprawl.club/@ludicity/113354467436519124">Ludic</a></p>
+
+  <p>The whole thing is clearly half-baked, written by someone who doesn't understand the meanings and reasons for things.</p>
+  <p class="attribution">-- <a href="https://news.ycombinator.com/item?id=41761873">Some dude on hackernews</a></p>
 </section>
 </div>
 
@@ -49,10 +66,31 @@
 }
   .dashboard section {
     padding: 1ch 1ch 2ch 1ch;
-    border-radius: 2px;
+  }
+  section.testimonial {
+    color: var(--bios-gray);
+    background-color: var(--bg-color) ;
+    font-size: 75%;
   }
 
+  section.testimonial p a {
+    color: var(--bios-gray);
+  }
+  
+  section.testimonial p {
+    line-height: 1rem;
+    margin-block-end: 1rem;
+    padding: .5ch;
+  }
+
+  section.testimonial p:hover {
+    color: blue;
+  }
   hr {
     border: 2px dashed var(--bg-color);
   }
+
+  .attribution {
+    text-align: right;
+}
 </style>
