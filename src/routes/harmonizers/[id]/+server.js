eb32cc93812ab2059e668dc116f2586ca1f51386
nikolaswise <nikolaswise@gmail.com>
2024-11-25 21:28:19 -0800

    stub out ring-defined harmonizer

---
+++ src/routes/harmonizers/[id]/+server.js
@@ -0,0 +1,19 @@
+import { json, error } from '@sveltejs/kit'
+
+import { instance } from '$env/static/private'
+let sample_harmonizer = (id) => {
+  return {
+    "@context": `${instance}context.json`,
+    "@id": `${instance}${id}`,
+    "@type": "Harmonizer",
+    "title": "Sample Microformat Harmonizer",
+    "term": `Hashtag`,
+    "selector": "[rel='category tag']",
+    "attribute": "href"
+  }
+}
+
+export async function GET(req) {
+  let harmonizerId = req.params.id
+  return json(sample_harmonizer(harmonizerId))
+}
