7795c719b8ac9b2d68d6a28389d91b190f46e197
nikolaswise <nikolaswise@gmail.com>
2025-05-06 21:16:11 -0700

    frame and get many

--- src/routes/debug/bookmarker/+server.js
+++ src/routes/debug/bookmarker/+server.js
@@ -1,9 +1,9 @@
 import { json, error } from '@sveltejs/kit'
 import {rdfa2triples} from '$lib/ld/rdfa2triples'
+import {asyncMap} from '$lib/asyncmap'
+import getRdfaGraph from 'graph-rdfa-processor'
 import context from '$lib/ld/context.json'
 import jsonld from 'jsonld'
-import getRdfaGraph from 'graph-rdfa-processor'
-import jsonldRdfaParser from 'jsonld-rdfa-parser'
 // TKTK import testHarmonizer for more fun
 import { JSDOM } from 'jsdom'
 import normalizeUrl from 'normalize-url'
@@ -27,12 +27,17 @@ const getBookmarks = async (input) => {
         })
       return obj
     })
-  const flat = await jsonld.compact(dataset, {
+  const smolctx = {
     "@base": "https://vocab.octothorp.es",
     "@vocab": "#",
     "type": "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
-    "Bookmark": "https://vocab.octothorp.es#Bookmark"
-  })
+    "Bookmark": "https://vocab.octothorp.es#Bookmark",
+    "bookmarks": {
+      "@id": "bookmarks",
+      "@type": "@id"
+    }
+  }
+  const flat = await jsonld.compact(dataset, smolctx)
   let bookmarks = flat['@graph']
     .filter(n => n["@id"] !== input)
   let blanks = bookmarks.map(n => n["@id"])
@@ -40,18 +45,32 @@ const getBookmarks = async (input) => {
     "@id": input,
     "bookmarks": blanks
   }, ...bookmarks]
-  return bookmarks
+  let framed = await jsonld.frame({
+    "@context": smolctx,
+    '@graph': bookmarks
+  }, {
+    '@id': input,
+    'bookmarks': {
+      '@id': {},
+      'url': {},
+      "octothorpes": {}
+    }
+  })
+  return framed
 }
 
 export async function GET({url}) {
   let defaultURL = 'https://demo.ideastore.dev'
-  let uri = url.searchParams.get('uri') ?? defaultURL
-  uri = new URL(uri)
-  let s = normalizeUrl(`${uri.origin}${uri.pathname}`)
-  let origin = normalizeUrl(uri.origin)
+  let uris = url
+    .searchParams
+    .get('uri')
+    .split(',') ?? [defaultURL]
+  let urls = uris
+    .map(url => new URL(url))
+    .map(url => normalizeUrl(`${url.origin}${url.pathname}`))
   let r
   try {
-    r = await getBookmarks(s)
+    r = await asyncMap(urls, getBookmarks)
   } catch (e) {
     console.log(e)
   }
