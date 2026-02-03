f300b352078b962fb8e2ab6d7c39d496fb64d5d8
Ním Daghlian <nim@ideastore.dev>
2026-02-03 15:38:46 -0800

    put cruft back in

--- src/routes/debug/badge-test/+server.js
+++ src/routes/debug/badge-test/+server.js
@@ -1,10 +1,5 @@
-import { readFileSync } from 'fs'
-import { resolve } from 'path'
-import { determineBadgeUri, badgeVariant } from '$lib/badge.js'
+import { determineBadgeUri } from '$lib/badge.js'
 import normalizeUrl from 'normalize-url'
-
-const badgeFile = 'badge.png'
-const badgeSuccess = readFileSync(resolve(`/static/${badgeFile}`))
 
 const pngHeaders = {
   'Content-Type': 'image/png',
@@ -23,7 +18,7 @@ function logRequest(entry) {
   }
 }
 
-export async function GET({ request, url }) {
+export async function GET({ request, url, fetch }) {
   // If ?log is present, return the log viewer
   if (url.searchParams.has('log')) {
     return new Response(renderLog(), {
@@ -86,7 +81,9 @@ export async function GET({ request, url }) {
     },
   })
 
-  return new Response(badgeSuccess, { headers: pngHeaders })
+  const badgeResponse = await fetch('/badge.png')
+  const badgeBuffer = await badgeResponse.arrayBuffer()
+  return new Response(badgeBuffer, { headers: pngHeaders })
 }
 
 function renderLog() {
